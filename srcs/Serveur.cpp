ec0e4dbfb6d5da024f6f2743873fde9bcec02d48
killian <killianscordel@gmail.com>
2024-04-09 12:24:58 +0200

    creation de l instance epoll

--- srcs/Serveur.cpp
+++ srcs/Serveur.cpp
@@ -2,24 +2,53 @@
 
 
 //Creer la socket utilisant le protocol AF_INET(internet ipv4) de type stream (TCP/IP)
-//on lui ajoute ensuite l option SO_REUSEADDR qui lui permet de reutiliser une adreese local
+//on la met en mode SO_REUSEADDR qui lui permet de reutiliser une adreese local
+//option level a 1 for ip
 
 void	Serveur::create_socket()
 {
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_fd == -1)
+	_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (_socket_fd == -1)
 		throw std::runtime_error("Error: Cannot creat socket");
 	int opt = 1;
-	if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+	if (setsockopt(_socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
 	{
-		close(socket_fd);
+		close(_socket_fd);
 		throw std::runtime_error("Error: Cannot set socket");
 	}
 }
 
-Serveur::Serveur(const int &port_, const std::string &password_) : port(port_), password(password_)
+
+
+void	Serveur::bind_socket()
+{
+	std::memset(&_sock_addr, 0, sizeof(sockaddr_in));
+	_sock_addr.sin_family = AF_INET;
+	_sock_addr.sin_addr.s_addr = INADDR_ANY;
+    _sock_addr.sin_port = htons(_port);
+	if (bind(_socket_fd, (struct sockaddr *)&_sock_addr, sizeof(_sock_addr) == -1))
+	{
+		close(_socket_fd);
+		throw std::runtime_error("Error: Cannot bind socket");
+	}
+}
+
+void	Serveur::create_epoll()
+{
+	
+}
+
+Serveur::Serveur(const int &port, const std::string &password) : _port(port), _password(password)
 {
 	create_socket();
+	bind_socket();
+	if (listen(_socket_fd, SOMAXCONN) == 1)
+	{
+		close(_socket_fd);
+		throw std::runtime_error("Error: Cannot listen to socket");
+	}
+	create_epoll();
+	
 }
 
 Serveur::~Serveur()
--- srcs/Serveur.hpp
+++ srcs/Serveur.hpp
@@ -10,18 +10,26 @@
 #include <unistd.h>
 
 #include <string>
+#include <cstring>
 #include <iostream>
 
 class Serveur
 {
  private:
 
-	int			port;
-	std::string	password;
-	int			socket_fd;
+	int					_port;
+	std::string			_password;
+	int					_socket_fd;
+	struct sockaddr_in	_sock_addr;
+
+	int					_epoll_fd;
+	struct epoll_event	_event;
+	
 
 
-	void create_socket();
+	void	create_socket();
+	void	bind_socket();
+	void	create_epoll();
  public:
 	Serveur(const int &port, const std::string &password_);
 	~Serveur();
